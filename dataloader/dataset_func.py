c614ebe8b24b509b52823da1fb74f8769d69478e
meenalparakh <meenalparakh18@gmail.com>
2023-07-09 16:31:46 -0700

    fixups and suggestions added

--- dataloader/dataset_func.py
+++ dataloader/dataset_func.py
@@ -30,6 +30,11 @@ def dataset_function() -> list[dict]:
             os.path.join(p, "color_images/color_image_4.png"),
         ]
 
+        # TODO (TP): fix the names below - variable name is depth but the files
+        # are color images
+        # TODO (TP): Try to write a for loop for this (both above and below)
+        # TODO (TP): a suggestion: use `import os, os.path as osp` so that you 
+        # can write os.path.join in short as osp.join
         other_depths = [
             os.path.join(p, "color_images/color_image_1.png"),
             os.path.join(p, "color_images/color_image_2.png"),
@@ -37,6 +42,7 @@ def dataset_function() -> list[dict]:
             os.path.join(p, "color_images/color_image_4.png"),
         ]
 
+        # TODO (TP): change it to get all 5000 data points
         current_dict = {
             "file_name": os.path.join(p, "color_images/color_image_0.png"),
             "depth_file_name": os.path.join(p, "depth_raw/depth_raw_0.npy"),
@@ -49,9 +55,7 @@ def dataset_function() -> list[dict]:
         }
 
         bboxes = generate_bbox(current_dict["seg_file_name"])
-        
-        # TODO (TP): change it to get all 5000 data points
-        
+                
         annotations = []
 
         grasp_collisions = data["grasp_collision"]
@@ -60,14 +64,17 @@ def dataset_function() -> list[dict]:
 
         obj_dims = data["obj_dims"]
         obj_types = data["obj_types"]
-        obj_poses = data["grasp_poses"]
+        obj_poses = data["obj_poses"]
 
         cam_int = data["intrinsic"]
         cam_poses = data["camera_poses"]
 
-        total_obs = len(grasp_poses)
+        total_grasps = len(grasp_poses)
 
-        for j in range(total_obs):
+        for j in range(total_grasps):
+
+            # TODO (TP): good to have a comment here for what these 
+            # two conditions are checking
             if (j+1 in bboxes) and (get_area(bboxes[j+1]) > 50):
                 
                 obj_pose = np.array(obj_poses[j])
@@ -77,31 +84,45 @@ def dataset_function() -> list[dict]:
                 grasp_width = np.array(grasp_widths[j])
                 grasp_collision = np.array(grasp_collisions[j])
 
+                # TODO (TP): the optional arguments to use for draw_on_image
+                # should have been left as comments for debugginf purposes.
+                # Thanks to one of the commits - I found what the arguments were.
+                # Thus, committing and pushing often - even for minor details - is helpful
                 result = generate_keypoints(
                     grasp_pose,
                     grasp_width,
                     cam_int,
                     cam_poses[0],
                     depth=np.load(current_dict["depth_file_name"]),
+                    draw=True,
+                    draw_info=(current_dict["file_name"], obj_type, i, None)
                 )
                 
-                if result is None:
+                projections_valid, ret, kpts_3d, kpts_2d = result
+                
+                valid = np.logical_and(projections_valid, (~grasp_collision))
+
+                if len(ret[valid]) == 0:
                     continue
                 
-                ret, kpts_3d, kpts_2d = result
-                if len(ret[~grasp_collision])==0:
-                    continue
+                # if len(ret[~grasp_collision])==0:
+                #     continue
                 
                 # TODO (TP): currently only first keypoint is taken
                 obj_dict = {
                     "obj_pose": obj_pose,
                     "obj_dim": obj_dim,
                     "obj_type": obj_type,
-                    "grasp_pose": grasp_pose[~grasp_collision],
-                    "grasp_width": grasp_width[~grasp_collision],
-                    "kpts_3d": kpts_3d[~grasp_collision],
-                    "kpts_2d": kpts_2d[~grasp_collision],
-                    "keypoints": ret[~grasp_collision][0], 
+                    "grasp_pose": grasp_pose[valid],
+                    "grasp_width": grasp_width[valid],
+                    "kpts_3d": kpts_3d[valid],
+                    "kpts_2d": kpts_2d[valid],
+                    "keypoints": ret[valid][0], 
+                    # "grasp_pose": grasp_pose[~grasp_collision],
+                    # "grasp_width": grasp_width[~grasp_collision],
+                    # "kpts_3d": kpts_3d[~grasp_collision],
+                    # "kpts_2d": kpts_2d[~grasp_collision],
+                    # "keypoints": ret[~grasp_collision][0], 
                     "bbox":list(bboxes[j+1]),
                     "bbox_mode": BoxMode.XYXY_ABS   
                 }
@@ -109,7 +130,6 @@ def dataset_function() -> list[dict]:
                 annotations.append(obj_dict)
 
         current_dict["annotations"] = annotations
-
         list_dict.append(current_dict)
 
     return list_dict
